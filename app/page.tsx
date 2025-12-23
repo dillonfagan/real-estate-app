7cd767565139c7d6a61b8818b519f65c72ca3fb2
Dillon Fagan <dillonfagan@pm.me>
2025-12-23 11:06:14 -0500

    Initial commit with property card.

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,91 @@
-import Image from "next/image";
+import PropertyCard from "@/components/property-card";
+import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
 
 export default function Home() {
+  const properties = [
+    {
+      imageUrl:
+        "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp",
+      title: "Property Title 1",
+      bedrooms: 2,
+      bathrooms: 2,
+      price: 1_200,
+    },
+    {
+      imageUrl:
+        "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp",
+      title: "Property Title 2",
+      bedrooms: 2,
+      bathrooms: 2,
+      price: 1_200,
+    },
+    {
+      imageUrl:
+        "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp",
+      title: "Property Title 3",
+      bedrooms: 2,
+      bathrooms: 2,
+      price: 1_200,
+    },
+    {
+      imageUrl:
+        "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp",
+      title: "Property Title 4",
+      bedrooms: 2,
+      bathrooms: 2,
+      price: 1_200,
+    },
+    {
+      imageUrl:
+        "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp",
+      title: "Property Title 5",
+      bedrooms: 2,
+      bathrooms: 2,
+      price: 1_200,
+    },
+    {
+      imageUrl:
+        "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp",
+      title: "Property Title 6",
+      bedrooms: 2,
+      bathrooms: 2,
+      price: 1_200,
+    },
+  ];
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main className="min-h-screen">
+      <div className="navbar bg-base-100 shadow-lg sticky top-0 z-50">
+        <div className="navbar-start">
+          <button className="btn btn-ghost font-bold text-lg text-red-600">
+            RedHouse
+          </button>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+        <div className="navbar-center">
+          <label className="input input-bordered">
+            <MagnifyingGlassIcon className="size-6" />
+            <input placeholder="Search" type="search" />
+          </label>
+        </div>
+        <div className="navbar-end"></div>
+      </div>
+      <div className="grid grid-cols-2 p-4 gap-4">
+        <div className="grid grid-cols-2 gap-4">
+          {properties.map((property, index) => (
+            <PropertyCard
+              key={index}
+              imageUrl={property.imageUrl}
+              title={property.title}
+              bedrooms={property.bedrooms}
+              bathrooms={property.bathrooms}
+              price={property.price}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          ))}
         </div>
-      </main>
-    </div>
+        <div className="overflow-hidden">
+          <div className="card bg-base-100">Placeholder</div>
+        </div>
+      </div>
+    </main>
   );
 }
---
+++ components/property-card.tsx
@@ -0,0 +1,24 @@
+interface Props {
+  imageUrl: string;
+  title: string;
+  bedrooms: number;
+  bathrooms: number;
+  price: number;
+}
+
+export default function PropertyCard({ imageUrl, title, bedrooms, bathrooms, price }: Props) {
+  return (
+    <div className="card bg-base-100 cursor-pointer shadow-sm hover:shadow-lg">
+      <figure>
+        <img src={imageUrl} alt={title} />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">${price}</h2>
+        <div className="flex gap-3">
+            <span>{bedrooms} beds</span>
+            <span>{bathrooms} baths</span>
+        </div>
+      </div>
+    </div>
+  );
+}

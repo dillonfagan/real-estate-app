0e652af9dad9625723d92c57188e516ad41c3bc8
Dillon Fagan <dillonfagan@pm.me>
2025-12-27 10:52:12 -0500

    Adjust how image is shown in property details drawer.

--- components/property-details.tsx
+++ components/property-details.tsx
@@ -10,7 +10,7 @@ export default function PropertyDetails({ property }: Props) {
   const { title, price, bedrooms, bathrooms, imageUrl, address } = property;
   return (
     <div>
-      <img src={imageUrl} alt={title} />
+      <img className="w-full h-64 object-cover" src={imageUrl} alt={title} />
       <div className="flex flex-col gap-2 p-4">
         <h3 className="font-semibold text-2xl">{title}</h3>
         <h4 className="font-semibold text-xl">
